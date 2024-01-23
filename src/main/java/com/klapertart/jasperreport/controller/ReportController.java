9ffecb37d63796ea2a7459cc0584f9563012e3e6
otong.sunandar <otong.sunandar@tritronik.com>
2024-01-23 13:17:53 +0700

    add: endpoint generateFile

--- src/main/java/com/klapertart/jasperreport/controller/ReportController.java
+++ src/main/java/com/klapertart/jasperreport/controller/ReportController.java
@@ -4,13 +4,23 @@ import com.klapertart.jasperreport.service.ReportService;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.function.ServerRequest;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 /**
@@ -58,5 +68,20 @@ public class ReportController {
         JasperPrint jasperPrint = reportService.nutrition();
         JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
     }
+
+    @GetMapping(value = "/nutrition/generateFile")
+    public ResponseEntity<byte[]> generateNutritionReport() throws IOException, JRException {
+        JasperPrint jasperPrint = reportService.nutrition();
+
+        byte[] bytes = JasperExportManager.exportReportToPdf(jasperPrint);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_PDF);
+        httpHeaders.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=nutrition.pdf");
+
+        return new ResponseEntity<>(bytes,httpHeaders,HttpStatus.OK);
+    }
+
+
 
 }
